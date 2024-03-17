84af07599a44e522b922b44f6e96e064ff21af8b
luizgabriel.tds1.senai@gmail.com <LuizGabrielLopesCarvalho>
2024-03-17 17:52:43 -0300

    Segundo metodo de array

---
+++ Metodo2Array.js
@@ -0,0 +1,25 @@
+//Exemplo 1 Array.pop
+
+let cidades = ['Valinhos', 'Campinas', 'Belo Horizonte']; //Crio o array com três cidades
+
+console.log(cidades); // Exibo as cidades do array
+
+let cidadesPop = cidades.pop(); // Utilizo o pop para remover a última cidade do array
+
+console.log(cidades); // Exibo as cidades após utilizar o pop, excluindo assim a última cidade
+
+console.log(cidadesPop); // Exibo qual cidade foi removida pelo pop
+
+console.log(''); // Espaço entre os exemplos
+console.log(''); // espaço entre os exemplos
+//Exemplo 2 Array.pop
+
+let cores = ['preto', 'branco', 'cinza']; //Crio o array com as cores
+
+console.log(cores); // Exibo todas as cores do array
+
+let coresPop = cores.pop(); // Utilizo o pop para remover a última cor
+
+console.log(cores); // Exibo as cores após a utilização do pop, excluindo então a última cor
+
+console.log(coresPop); // Exibo o nome da cor excluída pelo pop
