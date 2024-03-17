0d4f93af31c243de4ebcb303242e37c473c2132e
luizgabriel.tds1.senai@gmail.com <LuizGabrielLopesCarvalho>
2024-03-17 18:24:41 -0300

    Quarto metodo do array

---
+++ Metodo4Array.js
@@ -0,0 +1,20 @@
+//Exemplo 1 .length
+let animais = ['cachorro', 'gato', 'pinguim', 'dragao de komodo']; // Criei o array com animais
+
+console.log(animais);// Exibi o array com todos os animais
+
+let comprimento = animais.length; // Criei uma variavel comprimento e igualei ao array animais e o .length para mostrar o comprimento do array
+
+console.log(comprimento); // Exibi o comprimento do array
+
+//Exemplo 2 .length
+console.log('');//Espaço entre os exemplos
+console.log('');//Espaço entre os exemplos
+
+let letras = ['a', 'b', 'c', 'd', 'e']; // Criei o array com letras
+
+console.log(letras); // Exibi o array com todas as letras
+
+let comprimentoL = letras.length; //Criei uma variavel chamada comprimentoL e igualei ao array letras com o .length para mostrar o comprimento do array
+
+console.log(comprimentoL); // Exibi o comprimento do array
