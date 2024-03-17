c7adbd9109f6f9d226091120f8ab8badd7462eee
luizgabriel.tds1.senai@gmail.com <LuizGabrielLopesCarvalho>
2024-03-17 20:36:17 -0300

    Nono método de Array

---
+++ Metodo9Array.js
@@ -0,0 +1,20 @@
+//Exemplo 1 .unshift
+let carros = ['Mustang', 'Dodge Charger', 'Camaro']; // Criei o array com três carros
+
+console.log(carros); // Exibi o array com os três primeiros carros sendo Mustang, Dodge Charger, e Camaro
+
+carros.unshift('Lancer', 'Pagani', 'McLaren'); // Usei o carros .unshift para adicionar mais três elementos no ínicio do Array
+
+console.log(carros); // Exibi o novo array com os três primeiros carros sendo Lancer, Pagani, e McLaren
+
+//Exemplo 2 .unshift
+console.log(''); //Espaço entre os exemplos
+console.log(''); //Espaço entre os exemplos
+
+let pilotos = ['Ayrton Senna', 'Max Verstappen', 'Lewis Hamilton']; //Criei um array com três pilotos
+
+console.log(pilotos); // Exibi o array com os três primeiros pilotos sendo Ayrton Senna, Max Verstappen, e Lewis Hamilton
+
+pilotos.unshift('Lando Norris', 'Charles Leclerc', 'Fernando Alonso'); // Utilizei o pilotos .unshift para adicionar mais três elementos no ínicio do array
+
+console.log(pilotos); // Exibi o novo array sendo os três primeiros pilotos sendo Lando Norris, Charles Leclerc, Fernando Alonso
