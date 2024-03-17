811b41d2aeb4cc9eedbe68fc916be223395aa99c
luizgabriel.tds1.senai@gmail.com <LuizGabrielLopesCarvalho>
2024-03-17 18:12:40 -0300

    Terceiro metodo de array

---
+++ Metodo3Array.js
@@ -0,0 +1,20 @@
+//Exemplo 1 .reverse
+let sequencia = [1,2,3,4,5]; // Criei uma sequencia numerica de 1 a 5
+
+console.log(sequencia); // Exibi a sequencia 
+
+let sequenciaInversa = sequencia.reverse(); // Criei uma variavel chamada sequenciaInversa e igualei ao array original mas utilizando o .reverse
+
+console.log(sequenciaInversa); // Exibi a sequencia invertida
+
+//Exemplo 2.reverse
+console.log(''); //espaço entre os exemplos
+console.log('');//espaço entre os exemplos
+
+let nome = ['Luiz', 'Gabriel', 'Lopes', 'Carvalho'] // Criei um array com quatro nomes
+
+console.log(nome); // Exibi o array
+
+let nomeInverso = nome.reverse(); // Criei uma variavel chamada nomeInverso e igualei ao array original mas utilizando o .reverse
+
+console.log(nomeInverso); // Exibi o array com os nomes invertidos
