d3760fb03e5a02aa1e2ec92aab707e9b014310f7
luizgabriel.tds1.senai@gmail.com <LuizGabrielLopesCarvalho>
2024-03-17 18:38:25 -0300

    Quinto metodo de array

---
+++ Metodo5Array.js
@@ -0,0 +1,18 @@
+//Exemplo 1 .shift
+let plantas = ['Magnolias', 'Hortênsia', 'Ipê']; //Criei o array com o nome de plantas
+
+console.log(plantas); // Exibi o array com todas as plantas
+
+let plantas2 = plantas.shift(); // Criei uma variavel plantas2 e igualei ela ao array das plantas com o .shift para remover o primeiro elemento do array
+
+console.log(plantas2); // Exibi o elemento removido do array das plantas
+
+//Exemplo 2 ;shift
+
+let arvores = ['Carvalho', 'Coqueiro', 'Salgueiro']; // Criei o array com o nome de três arvores
+
+console.log(arvores); // Exibi o array com todas as arvores
+
+let arvores2 = arvores.shift(); // Criei uma variavel arvores2 e igualei ela ao array das arvores com o .shift pra remover o primeiro elemento do array
+
+console.log(arvores2); //Exibi o elemento removido do array das arvores
