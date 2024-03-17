e3af25e2d8858916e0cbd92746a8f6a9b4f9c06a
luizgabriel.tds1.senai@gmail.com <LuizGabrielLopesCarvalho>
2024-03-17 20:01:43 -0300

    Oitavo método de Array

---
+++ Metodo8Array.js
@@ -0,0 +1,20 @@
+//Exemplo 1 .push
+let país = ['Inglaterra', 'França', 'EUA']; // Criei um array com 3 países 
+
+console.log(país); // Exibi os países do array
+
+país.push('Alemanha', 'Japão', 'Itália'); // Usei o .push para adicionar mais 3 países ao array
+
+console.log(país); // Exibi o array com todos os países inclusive os novos adicionados pelo push
+
+//Exemplo 2 .push
+console.log(''); //Espaço entre os exemplos
+console.log(''); //Espaço entre os exemplos
+
+let presidente = ['Churchill', 'Lebrun', 'Roosevelt']; // Criei o array com 3 presidentes
+
+console.log(presidente); // Exibi o array com os 3 primeiros presidentes
+
+presidente.push('Hitler', 'Hirohito', 'Mussolini'); // Utilizei o .push para adicionar mais 3 presidentes ao array
+
+console.log(presidente); // Exibi o novo array com os 6 presidentes
