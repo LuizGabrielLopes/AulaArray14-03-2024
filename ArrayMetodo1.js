7b71deba6b12b89df8015edcf12e1c968641c43b
luizgabriel.tds1.senai@gmail.com <LuizGabrielLopesCarvalho>
2024-03-17 17:09:25 -0300

    first commit

---
+++ ArrayMetodo1.js
@@ -0,0 +1,23 @@
+//exemplo 1 concat
+
+let array1 = ['0', '1', '2']; //Criação do primeiro array
+
+let array2 = ['3', '4', '5']; //Criação do segundo array
+
+let array3 = array1.concat(array2); //Criação do terceiro array e igualando ele à junção dos dois primeiros arrays
+
+console.log(array3); //Exibição do array3
+
+//Coloquei um espaço para separar os dois exemplos
+console.log('')
+console.log('')
+
+//exemplo 2 concat
+
+let array4 = ['abacaxi', 'melão', 'maça'];// Criação do quarto array
+
+let array5 = ['maça', 'maracujá', 'laranja']; //Criação do quinto array
+
+let array6 = array4.concat(array5); // Criação do sexto array e igualando ele à junção dos outros dois arrays anteriores
+
+console.log(array6);//Exibição do sexto array
