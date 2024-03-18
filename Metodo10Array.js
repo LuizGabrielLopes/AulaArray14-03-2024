054ff5a31d38bedf29316f39ed0fe8ea71e58577
luizgabriel.tds1.senai@gmail.com <LuizGabrielLopesCarvalho>
2024-03-17 21:22:24 -0300

    Décimo método de Array

---
+++ Metodo10Array.js
@@ -0,0 +1,20 @@
+//Exemplo 1 .join
+let alien = ['Friagem', 'Bloxx', 'EcoEco']; // Criei um array com três elementos
+
+console.log(alien.join()); // Utilizei o .join para juntar os elementos da String com uma vírgula entre eles
+
+console.log(alien.join('_')); // Utilizei o .join para juntar os elementos da String com um underline entre eles
+
+console.log(alien.join('-')); // Utilizei o .join para juntar os elementos da String com um hífen entre eles
+
+//Exemplo 2 .join
+console.log(''); //Espaço entre os exemplos
+console.log(''); //Espaço entre os exemplos
+
+let pokemon = ['Ash Greninja', 'Charizard', 'Lucario']; //Criei um array com três elementos
+
+console.log(pokemon.join(  )); // Utilizei o .join para juntar os elementos da String com uma vírgula entre eles
+
+console.log(pokemon.join('   ')); // Utilizei o .join para juntar os elementos da String com espaço entre eles
+
+console.log(pokemon.join('____')); // Utilizei o .join para juntar os elementos da String com underlines entre eles
